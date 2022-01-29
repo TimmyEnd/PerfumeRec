0d9e25a675cbd26125f739cb65f0c078b74acae7
TimmyEnd <93699629+TimmyEnd@users.noreply.github.com>
2022-01-29 15:50:40 +0800

    Add files via upload

--- st.py
+++ st.py
@@ -5,6 +5,7 @@ import networkx as nx
 import matplotlib.pyplot as plt
 from matplotlib.pyplot import figure
 import spacy
+import en_core_web_lg
 nlp = spacy.load("en_core_web_lg")
 import random
 from PIL import Image
